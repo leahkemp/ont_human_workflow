3ca83d2c1fc168fe263a3eb63d3e91165e186aeb
Leah Kemp <leah.kemp@esr.cri.nz>
2022-12-13 13:52:02 +1300

    add cleanup to script

--- scripts/module_scripts/01-cthulhu-guppy-gpu.sh
+++ scripts/module_scripts/01-cthulhu-guppy-gpu.sh
@@ -18,6 +18,9 @@ GUPPYPATH="/opt/admin/dsc/guppy-gpu/6.4.2/ont-guppy/bin/"
 CONFIG="dna_r10.4.1_e8.2_400bps_modbases_5mc_cg_sup_prom.cfg"
 REF="/NGS/clinicalgenomics/public_data/ncbi/GRCh38/GCA_000001405.15_GRCh38_no_alt_analysis_set.fasta.gz"
 
+# cleaup old ouputs of this script to avoid writing to file twice
+rm -rf ${WKDIR}/results/01-cthulhu-guppy-gpu/${SAMPLE}/
+
 # create output directory if it doesn't yet exist
 mkdir -p ${WKDIR}/results/01-cthulhu-guppy-gpu/${SAMPLE}/
 
