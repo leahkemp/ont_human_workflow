13463b4f9d11a24d5b048d294104e21f0ab3c757
Leah Kemp <leah.kemp@esr.cri.nz>
2022-12-15 12:34:23 +1300

    put outputs of script in better location

--- scripts/module_scripts/07-ont-wf-human-cnv.sh
+++ scripts/module_scripts/07-ont-wf-human-cnv.sh
@@ -17,11 +17,11 @@ REF="/NGS/clinicalgenomics/public_data/ncbi/GRCh38/GCA_000001405.15_GRCh38_no_al
 
 # cleaup old ouputs of this script to avoid writing to file twice
 rm -rf "${WKDIR}"/results/03-ont-human-variation-calling/"${SAMPLE}"/
-rm rf "${WKDIR}"/results/01-ont-guppy-gpu/"${SAMPLE}"/pass/fastq/
+rm rf "${WKDIR}"/results/07-ont-wf-human-cnv/"${SAMPLE}"/fastq/
 
 # create output directory if it doesn't yet exist
 mkdir -p "${WKDIR}"/results/07-ont-wf-human-cnv/"${SAMPLE}"/work/
-mkdir -p "${WKDIR}"/results/01-ont-guppy-gpu/"${SAMPLE}"/pass/fastq/ 
+mkdir -p "${WKDIR}"/results/07-ont-wf-human-cnv/"${SAMPLE}"/fastq/ 
 
 # set the shell to be used by conda for this script (and re-start shell to implement changes)
 conda init bash
@@ -36,7 +36,7 @@ mamba env create \
 conda activate nextflow.22.10.1
 
 # put fastq files in a single directory named "fastq" - Miles notes the pipeline seems to require this
-rsync -av "${WKDIR}"/results/01-ont-guppy-gpu/"${SAMPLE}"/pass/*.fastq "${WKDIR}"/results/01-ont-guppy-gpu/"${SAMPLE}"/pass/fastq/
+rsync -av "${WKDIR}"/results/01-ont-guppy-gpu/"${SAMPLE}"/pass/*.fastq "${WKDIR}"/results/07-ont-wf-human-cnv/"${SAMPLE}"/fastq/ 
 
 # run copy number variant calling
 nextflow run -c "${WKDIR}"/config/07-ont-wf-human-cnv/nextflow.config epi2me-labs/wf-cnv \
@@ -47,7 +47,7 @@ nextflow run -c "${WKDIR}"/config/07-ont-wf-human-cnv/nextflow.config epi2me-lab
 -with-timeline \
 -with-trace \
 -resume \
---fastq "${WKDIR}"/results/01-ont-guppy-gpu/"${SAMPLE}"/pass/fastq/ \
+--fastq "${WKDIR}"/results/07-ont-wf-human-cnv/"${SAMPLE}"/fastq/  \
 --sample_sheet "${WKDIR}"/config/07-ont-wf-human-cnv/wf_human_cnv_sample_sheet.csv \
 --fasta "${REF}" \
 --genome hg38 \
@@ -59,8 +59,8 @@ nextflow run -c "${WKDIR}"/config/07-ont-wf-human-cnv/nextflow.config epi2me-lab
 cd "${WKDIR}"
 
 # Notes:
-# EPI2ME Labs workflow for ONT CNV analysis: https://github.com/epi2me-labs/wf-cnv
-#  CNV pipeline will run on the basecalled fastq files, it will run an alignment against 
+# EPI2ME Labs workflow for ONT CNV analysis: ht`tps://github.com/epi2me-labs/wf-cnv
+# CNV pipeline will run on the basecalled fastq files, it will run an alignment against 
 # the provided reference genome, and then perform CNV analysis using QDNAseq.
 
 # there is currently a fun little issue with the CNV pipeline needing a sample sheet
