22aaef010e0fc8177eeeb6323508a5992aaf3783
Leah Kemp <leah.kemp@esr.cri.nz>
2022-12-15 12:28:40 +1300

    add cleanup

--- scripts/module_scripts/07-ont-wf-human-cnv.sh
+++ scripts/module_scripts/07-ont-wf-human-cnv.sh
@@ -15,6 +15,10 @@ SAMPLE="OM1052A"
 WKDIR="/NGS/humangenomics/active/2022/run/ont_human_workflow/"
 REF="/NGS/clinicalgenomics/public_data/ncbi/GRCh38/GCA_000001405.15_GRCh38_no_alt_analysis_set.fasta.gz"
 
+# cleaup old ouputs of this script to avoid writing to file twice
+rm -rf "${WKDIR}"/results/03-ont-human-variation-calling/"${SAMPLE}"/
+rm rf "${WKDIR}"/results/01-ont-guppy-gpu/"${SAMPLE}"/pass/fastq/
+
 # create output directory if it doesn't yet exist
 mkdir -p "${WKDIR}"/results/07-ont-wf-human-cnv/"${SAMPLE}"/work/
 mkdir -p "${WKDIR}"/results/01-ont-guppy-gpu/"${SAMPLE}"/pass/fastq/ 
